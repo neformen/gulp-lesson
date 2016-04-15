3d2e8085a0487bc5d3baf352f992f7517d7eaed9
tamango92 <ar.arkhipov@gmail.com>
2016-04-15 11:35:53 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 40,
+    setWrapperSize: true,
+    slidesPerView: 'auto',
+    loopedSlides: 6,
+    centeredSlides: true,
+    initialSlide: 1,
+    loop: true,
+    autoplay: 5000,
+    speed: 500,
+    DOMAnimation: false
+});
+
